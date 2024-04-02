1027cfb3d58c3e01723afa9652b55eaaf1326fec
matteocenci <matteocenci01@gmail.com>
2024-04-02 20:05:01 +0200

    feat: fifth jsnack done

---
+++ JSnack-5/js/script5.js
@@ -0,0 +1,11 @@
+
+
+let double = 1;
+
+
+//stampare le potenze fino a mille
+// cercando su internet ho trovato l'utilizzo di while
+while (double <= 1000) {
+    console.log(double);
+    double *= 2;
+}
