4e845f51618775dc33bdb3ac0a1123a8ad5c3cf8
matteocenci <matteocenci01@gmail.com>
2024-04-02 15:53:24 +0200

    feat: init project

---
+++ JSnack-1/js/script1.js
@@ -0,0 +1,31 @@
+// chiediamo all'utente di darci un numero per dieci volte
+
+const firstNumber = parseInt(prompt("Okay ora ti chiederò di darmi dieci numeri, iniziamo dal primo:"))
+console.log(firstNumber);
+
+const secondNumber = parseInt(prompt("Dammi il secondo:"));
+console.log(secondNumber);
+
+const thirdNumber = parseInt(prompt("Il terzo:"));
+console.log(thirdNumber);
+
+const fourthNumber = parseInt(prompt("Il quarto:"));
+console.log(fourthNumber);
+
+const fifthNumber = parseInt(prompt("Il quinto:"));
+console.log(fifthNumber);
+
+const sixthNumber = parseInt(prompt("Il sesto:"));
+console.log(sixthNumber);
+
+const seventhNumber = parseInt(prompt("Il settimo:"));
+console.log(seventhNumber);
+
+const eightNumber = parseInt(prompt("L'ottavo:"));
+console.log(eightNumber);
+
+const ninthNumber = parseInt(prompt("Il nono:"));
+console.log(ninthNumber);
+
+const tenthNumber = parseInt(prompt("Ed infine il decimo!"))
+console.log(tenthNumber);
